346e72d530bcf4e877f5071c4adc5f8257ac9ee1
scortina <stycormar@gmail.com>
2015-09-18 16:37:49 -0500

    ...

--- application/controllers/cxc.php
+++ application/controllers/cxc.php
@@ -1,32 +1,32 @@
 <?php
 class Cxc extends CI_Controller {
-	public function __construct(){
-		parent::__construct();
-		$this->pagina = '';
-		$timezone = @date_default_timezone_get();
-       if (!isset($timezone) || $timezone == '') { $timezone = @ini_get('date.timezone'); }
-       if (!isset($timezone) || $timezone == '') { $timezone = 'UTC'; }
-       date_default_timezone_set($timezone); 
-		$this->load->database();
-		$this->load->helper('url');
-		$this->load->library('grocery_CRUD');
-		$this->load->model('datos_model');
-	}
+    public function __construct(){
+            parent::__construct();
+            $this->pagina = '';
+            $timezone = @date_default_timezone_get();
+   if (!isset($timezone) || $timezone == '') { $timezone = @ini_get('date.timezone'); }
+   if (!isset($timezone) || $timezone == '') { $timezone = 'UTC'; }
+   date_default_timezone_set($timezone); 
+            $this->load->database();
+            $this->load->helper('url');
+            $this->load->library('grocery_CRUD');
+            $this->load->model('datos_model');
+    }
 
-	function pendientes(){
-		if($this->session->userdata('is_logged_in')){
-			$data['titulo'] = "Pendientes CXC";
-			$data['calendario'] = true;
-			$this->load->view('include/head');
-			$this->load->view('include/header',$data);
-			$this->load->view('admin/html_cxc');
-			$this->load->view('admin/librerias');
-			$this->load->view('admin/cxc');
-			$this->load->view('include/footer');
-		} else {
-			$this->load->view('admin/login');	
-		}
-	}
+    function pendientes(){
+            if($this->session->userdata('is_logged_in')){
+                    $data['titulo'] = "Pendientes CXC";
+                    $data['calendario'] = true;
+                    $this->load->view('include/head');
+                    $this->load->view('include/header',$data);
+                    $this->load->view('admin/html_cxc');
+                    $this->load->view('admin/librerias');
+                    $this->load->view('admin/cxc');
+                    $this->load->view('include/footer');
+            } else {
+                    $this->load->view('admin/login');	
+            }
+    }
         
     function efectivo(){
 		if($this->session->userdata('is_logged_in')){
@@ -41,48 +41,31 @@ class Cxc extends CI_Controller {
 			$this->load->view('admin/login');	
 		}
 	}
-     
+    
     function facturas(){
 		if($this->session->userdata('is_logged_in')){
 			$data['titulo'] = "Facturas";
 			$data['calendario'] = false;
-			$this->load->view('include/head');
-			$this->load->view('include/header',$data);
-			$this->load->view('admin/html_cxc_facturas');
-			$this->load->view('admin/librerias');
-			$this->load->view('admin/cxc');
-			$this->load->view('include/footer');
+			$this->load->view('Jincludes/head');
+			$this->load->view('Jincludes/header',$data);
+			$this->load->view('admin/Factura/html_cxc_facturas');
+			$this->load->view('Jincludes/footer');
+			$this->load->view('Jincludes/librerias');
 		} else {
 			$this->load->view('admin/login');	
 		}
 	}
-        function grupo(){
-		if($this->session->userdata('is_logged_in')){
-			$data['titulo'] = "Grupo";
-			$data['calendario'] = false;
-			$this->load->view('include/head');
-			$this->load->view('include/header',$data);
-			$this->load->view('admin/html_cxc_grupos');
-			$this->load->view('admin/librerias');
-			$this->load->view('admin/cxc');
-			$this->load->view('include/footer');
-		} else {
-			$this->load->view('admin/login');	
-		}
-	}
-        function pagos(){
-		if($this->session->userdata('is_logged_in')){
-			$data['titulo'] = "Pagos";
-			$data['calendario'] = false;
-			$this->load->view('include/head');
-			$this->load->view('include/header',$data);
-			$this->load->view('admin/html_cxc_pagos');
-			$this->load->view('admin/librerias');
-			$this->load->view('admin/cxc');
-			$this->load->view('include/footer');
-		} else {
-			$this->load->view('admin/login');	
-		}
-	}
-
+    function grupo(){
+            if($this->session->userdata('is_logged_in')){
+                    $data['titulo'] = "Grupo";
+                    $data['calendario'] = false;
+                    $this->load->view('Jincludes/head');
+                    $this->load->view('Jincludes/header',$data);
+                    $this->load->view('admin/Grupo/html_cxc_grupos');
+                    $this->load->view('Jincludes/footer');
+                    $this->load->view('Jincludes/librerias');
+            } else {
+                    $this->load->view('admin/login');	
+            }
+    }
 }
---
+++ application/views/admin/Grupo/html_cxc_grupos.php
@@ -0,0 +1,334 @@
+
+<div class="portlet box grey">
+    <div class="portlet-body form">
+        <form action="#" class="form-horizontal">
+            <div class="form-body">
+                <h3 class="form-section">Buscar R.P</h3>
+                <div class="row">
+                    <div class="span6">
+                        <div class="input-append">
+                            <label>R.P</label>
+                            <input class="span4" id="txtRepresentante" placeholder="Representante" type="text">
+                            <button class="btn" type="button"><i class="icon icon-search"></i></button>
+                        </div>
+                    </div>
+                    <!--/span-->
+                    <div class="span6">
+                        <div class="input-prepend input-append">
+                            <label>Saldo Disponible</label>
+                            <span class="add-on">$</span>
+                            <input class="span4 uneditable-input" id="txtSaldo" placeholder="Saldo Disponible" type="text">
+                            <span class="add-on">.00</span>
+                        </div>
+                    </div>
+                <!--/span-->
+              </div>
+                <!--/row-->
+            </div>
+        </form>
+    </div>  
+</div>
+<div class="portlet box grey">
+    <div class="portlet-body form">
+        <h3 class="form-section">Listado de Saldos del cliente</h3>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th style="text-align:center;">
+                        <div class="floatL t5">
+                            <input type="checkbox" class="select-all">
+                        </div>
+                    </th>
+                    <th>Fecha</th>
+                    <th class="hidden-xs">Lugar</th>
+                    <th class="hidden-xs">Cliente</th>
+                    <th>Pax</th>
+                    <th>Importe</th>    
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>  
+</div>
+<div class="portlet box grey">
+    <div class="portlet-body form">
+        <h3 class="form-section">Formas de Pago</h3>
+        <div class="tabbable"> <!-- Only required for left/right tabs -->
+            <ul class="nav nav-tabs">
+                <li><a href="#tab1" data-toggle="tab">Efectivo</a></li>
+                <li><a href="#tab2" data-toggle="tab">Dolares</a></li>
+                <li class="active"><a href="#tab3" data-toggle="tab">Tarjeta C.</a></li>
+                <li><a href="#tab4" data-toggle="tab">Canadience</a></li>
+                <li><a href="#tab5" data-toggle="tab">Euro</a></li>
+                <li><a href="#tab6" data-toggle="tab">Cupon</a></li>
+                <li><a href="#tab7" data-toggle="tab">Transferencia</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane" id="tab1">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="tab2">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane active" id="tab3">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            <div class="control-group">
+                                <label style="visibility:hidden;">Cantidad</label>
+                                <button class="btn" title="Agregar" type="button"><i class="icon icon-plus"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span10">
+                           <table class="table table-bordered" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th class="hidden-xs">Lugar</th>
+                                        <th>Fecha</th>
+                                        <th>Cantidad</th>   
+                                        <th style="text-align:center;">
+                                            <div class="floatL t5">
+                                                <input type="checkbox" class="select-all">
+                                            </div>
+                                        </th> 
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="span1">
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="tab4">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="tab5">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="tab6">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="tab7">
+                    <div class="row">
+                        <div class="span1">
+                           
+                        </div>
+                        <div class="span4">
+                            <div class="control-group">
+                                <label>Lugar</label>
+                                <input class="input-block-level" id="txtLugar" placeholder="Lugar" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span3">
+                            <div class="control-group">
+                                <label>Fecha</label>
+                                <input class="input-block-level" id="txtFecha" placeholder="Fecha" type="text">
+                            </div>
+                        </div> 
+                        <div class="span3">
+                            <div class="input-prepend input-append">
+                                <label>Cantidad</label>
+                                <span class="add-on">$</span>
+                                <input class="input-block-level" id="txtCantidad" placeholder="Cantidad" type="text">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="span1">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="span10">
+                <div class="control-group">
+                    <label style="">Observaciones</label>
+                    <textarea class="input-block-level" id="observaciones" rows="1"></textarea>
+                </div>
+
+            </div>
+            <div class="span2">
+                <label style="visibility:hidden;">Cantidad</label>
+                <button class="btn input-block-level btn-large" title="Guardar" type="button">Pagar</button>           
+            </div>
+        </div>
+
+    </div>  
+</div>
